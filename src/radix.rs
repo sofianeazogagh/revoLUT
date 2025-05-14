f422f604b4f8a514eaa25f27ca93981a65dc839a
sofianeazogagh <sofiane.a.f@hotmail.fr>
2025-05-14 22:16:21 +0200

    blind array maybe inc or dec

--- src/radix.rs
+++ src/radix.rs
@@ -139,6 +139,19 @@ impl NyblByteLUT {
         self.blind_array_set(index, &new_value, ctx, public_key);
     }
 
+    /// increment, decrement or do nothing to encrypted bytevalue at encrypted nyble index
+    pub fn blind_array_maybe_inc_or_dec(
+        &mut self,
+        index: &LWE,
+        b: &LWE,
+        ctx: &Context,
+        public_key: &PublicKey,
+    ) {
+        let current = self.blind_array_access(index, ctx, public_key);
+        let new_value = public_key.byte_lwe_maybe_inc_or_dec(&current, &b, ctx);
+        self.blind_array_set(index, &new_value, ctx, public_key);
+    }
+
     /// increment encrypted byte value at encrypted nybl index
     pub fn blind_array_inc(&mut self, index: &LWE, ctx: &Context, public_key: &PublicKey) {
         // 2 br
@@ -419,6 +432,51 @@ impl PublicKey {
 
         // Third round of switch case
         let hi = self.switch_case3(&a.hi, &s, &vec![lut_id, lut_inc, lut_dec], ctx);
+
+        ByteLWE { lo, hi }
+    }
+
+    // FIXME : this is not working
+    pub fn dirty_byte_lwe_maybe_inc_or_dec(&self, a: &ByteLWE, b: &LWE, ctx: &Context) -> ByteLWE {
+        let p = ctx.full_message_modulus as u64;
+
+        // First round of switch case
+        // Identity luts
+        let lut_id = LUT::from_vec_trivially(&(0..p).collect::<Vec<_>>(), ctx);
+        // Increment lut
+        let lut_inc =
+            LUT::from_vec_trivially(&(0..p).map(|x| (x + 1) % p).collect::<Vec<_>>(), ctx);
+        // Decrement lut
+        let lut_dec =
+            LUT::from_vec_trivially(&(0..p).map(|x| (x - 1) % p).collect::<Vec<_>>(), ctx);
+
+        let mut lo = a.lo.clone();
+        lwe_ciphertext_add_assign(&mut lo, &b);
+
+        // Second round of switch case
+        // Instanciate the luts
+        let zeros = vec![0; p as usize];
+        let mut vec_last = zeros.clone();
+        vec_last[p as usize - 1] = 1;
+        let mut vec_first = zeros.clone();
+        vec_first[0] = p - 1;
+
+        let lut_z = LUT::from_vec_trivially(&zeros, ctx); // [0,..,0]
+        let lut_last = LUT::from_vec_trivially(&vec_last, ctx); // [0,..,0,1]
+        let lut_first = LUT::from_vec_trivially(&vec_first, ctx); // [F,0,..,0]
+
+        let mut luts = vec![lut_z.clone(), lut_last.clone()];
+        luts.extend(vec![lut_z.clone(); (p - 3) as usize]);
+        luts.push(lut_first.clone());
+        assert_eq!(luts.len(), p as usize);
+        let s = self.switch_case3(&a.lo, &b, &luts, ctx);
+
+        // Third round of switch case
+        let mut luts = vec![lut_id.clone(), lut_inc.clone()];
+        luts.extend(vec![lut_z.clone(); (p - 3) as usize]);
+        luts.push(lut_dec.clone());
+        assert_eq!(luts.len(), p as usize);
+        let hi = self.switch_case3(&a.hi, &s, &luts, ctx);
 
         ByteLWE { lo, hi }
     }
@@ -852,5 +910,21 @@ mod test {
             b as u64,
             actual
         );
+    }
+
+    #[test]
+    fn test_dirty_byte_lwe_maybe_inc_or_dec() {
+        let mut ctx = Context::from(PARAM_MESSAGE_4_CARRY_0);
+        let private_key = key(ctx.parameters);
+        let public_key = &private_key.public_key;
+
+        let a = 0x20;
+        let b = 0x0F;
+
+        let enc_a = ByteLWE::from_byte(a, &mut ctx, private_key);
+        let enc_b = private_key.allocate_and_encrypt_lwe(b, &mut ctx);
+
+        let c = public_key.dirty_byte_lwe_maybe_inc_or_dec(&enc_a, &enc_b, &ctx);
+        println!("c: {:02X}", c.to_byte(&ctx, private_key));
     }
 }
